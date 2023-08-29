c38af39134807e8947081be4785488497858aef2
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:23:23 +0400

    Update FlightController.java

--- src/main/java/controller/FlightController.java
+++ src/main/java/controller/FlightController.java
@@ -1,4 +1,25 @@
 package controller;
 
 public class FlightController {
+  private final FlightServiceImpl flightService;
+
+    public FlightController(FlightServiceImpl flightService) {
+        this.flightService = flightService;
+    }
+
+    public List<Flight> getAllMyFlights(String name, String surName) {
+        return flightService.getALlMyFlights(name, surName);
+
+    }
+
+    public List<Flight> searchTicket(String destination, int seats) {
+        return flightService.searchTicket(destination,seats);
+    }
+    public List<Flight> getAllFlightsFromOneOfCity(String departureCity) {
+        return flightService.getALlFlightsFromOneOfCity(departureCity);
+    }
+
+    public Flight getFlightsByFlightNumber(String flightNumber) {
+        return flightService.getFlightsByFlightNumber(flightNumber);
+    }
 }
