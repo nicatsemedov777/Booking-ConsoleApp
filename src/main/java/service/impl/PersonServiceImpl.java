526bd02f2a800032dca92cf0fe6ae49cd1c02667
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:35:35 +0400

    Update PersonServiceImpl.java

--- src/main/java/service/impl/PersonServiceImpl.java
+++ src/main/java/service/impl/PersonServiceImpl.java
@@ -1,4 +1,14 @@
 package service.impl;
 
 public class PersonServiceImpl {
+    private final PersonDao personDao;
+
+    public PersonServiceImpl(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @Override
+    public List<Person> getAllPerson() {
+        return personDao.getAllPerson();
+    }
 }
