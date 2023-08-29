ea43cf558660d2f3b53409f2a7abe6acfa9674d6
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:28:46 +0400

    Update ConnectionSql.java

--- src/main/java/db/ConnectionSql.java
+++ src/main/java/db/ConnectionSql.java
@@ -1,4 +1,31 @@
 package db;
 
 public class ConnectionSql {
+      private static final String url = "jdbc:postgresql://localhost:5432/booking_ticket";
+    private static final String username = "postgres";
+    private static final String password = "1903";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
