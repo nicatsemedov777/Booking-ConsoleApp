16dc778586ff7118866dca00551a8b900c8c0056
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:22:39 +0400

    Update BookingController.java

--- src/main/java/controller/BookingController.java
+++ src/main/java/controller/BookingController.java
@@ -1,4 +1,13 @@
 package controller;
 
 public class BookingController {
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    public void cancelBooking(String bookingNumber) {
+         bookingService.cancelBooking(bookingNumber);
+    }
 }
