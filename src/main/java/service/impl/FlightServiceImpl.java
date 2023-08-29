eb5a0b7b1d561aa80f5b797f33d9daee2f36c592
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:34:59 +0400

    Update FlightServiceImpl.java

--- src/main/java/service/impl/FlightServiceImpl.java
+++ src/main/java/service/impl/FlightServiceImpl.java
@@ -1,4 +1,29 @@
 package service.impl;
 
 public class FlightServiceImpl {
+    private final FlightDaoImpl flightDao;
+
+    public FlightServiceImpl(FlightDaoImpl flightDao) {
+        this.flightDao = flightDao;
+    }
+
+    @Override
+    public List<Flight> getALlMyFlights(String name,String surName) {
+        return flightDao.getALlMyFlights(name,surName);
+    }
+
+    @Override
+    public Flight getFlightsByFlightNumber(String flightNumber) {
+        return flightDao.getFlightsByFlightNumber(flightNumber);
+    }
+
+    @Override
+    public List<Flight> getALlFlightsFromOneOfCity(String departureCity) {
+        return flightDao.getALlFlightsFromOneOfCity(departureCity);
+    }
+
+    @Override
+    public List<Flight> searchTicket(String destination, int seats) {
+        return flightDao.searchTicket(destination,seats);
+    }
 }
