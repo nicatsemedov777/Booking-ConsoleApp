31fad5394eb0df6b4e9a8dd379bfd5745ad02bb5
Farid Malikov <127598880+FaridMaIikov@users.noreply.github.com>
2023-08-30 00:25:15 +0400

    Update FlightDao.java

--- src/main/java/dao/FlightDao.java
+++ src/main/java/dao/FlightDao.java
@@ -1,4 +1,11 @@
 package dao;
 
 public interface FlightDao  {
+    List<Flight> getALlMyFlights(String name, String surName);
+
+    Flight getFlightsByFlightNumber(String flightNumber);
+
+    List<Flight> getALlFlightsFromOneOfCity(String departureCity);
+
+    List<Flight> searchTicket(String destination,int seats);
 }
